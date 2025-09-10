3b9ba8329b12aa7777b7a587702ad13f28e68122
Randy <randydhondt@live.nl>
2025-09-10 13:48:02 +0200

    lint fixes

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -1,32 +1,28 @@
-import React from 'react';
-import Link from 'next/link';
-import { ArrowRight, Globe, Shield, Users, Key, Database, Clock } from 'lucide-react';
-import AuthAwareButtons from '@/components/AuthAwareButtons';
 import { redirect } from "next/navigation";
 
 export default function Home() {
 
   redirect("/app");
 
-  const productName = process.env.NEXT_PUBLIC_PRODUCTNAME;
+  // const productName = process.env.NEXT_PUBLIC_PRODUCTNAME;
 
-  return (
-      <div className="min-h-screen">
-        <nav className="fixed top-0 w-full bg-white/80 backdrop-blur-sm z-50 border-b border-gray-100">
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div className="flex justify-between h-16 items-center">
-              <div className="flex-shrink-0">
-              <span className="text-2xl font-bold bg-gradient-to-r from-primary-600 to-primary-500 bg-clip-text text-transparent">
-                {productName}
-              </span>
-              </div>
-              <div className="hidden md:flex items-center space-x-8">
-                <AuthAwareButtons variant="nav" />
-              </div>
-            </div>
-          </div>
-        </nav>
+  // return (
+  //     <div className="min-h-screen">
+  //       <nav className="fixed top-0 w-full bg-white/80 backdrop-blur-sm z-50 border-b border-gray-100">
+  //         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+  //           <div className="flex justify-between h-16 items-center">
+  //             <div className="flex-shrink-0">
+  //             <span className="text-2xl font-bold bg-gradient-to-r from-primary-600 to-primary-500 bg-clip-text text-transparent">
+  //               {productName}
+  //             </span>
+  //             </div>
+  //             <div className="hidden md:flex items-center space-x-8">
+  //               <AuthAwareButtons variant="nav" />
+  //             </div>
+  //           </div>
+  //         </div>
+  //       </nav>
 
-      </div>
-  );
+  //     </div>
+  // );
 }
